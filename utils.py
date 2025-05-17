900f7ed715a7b16c6a119bbc53d347a8520684f0
Fortuna233 <1311722974@qq.com>
2025-05-17 15:48:03 +0800

    last commit

--- utils.py
+++ utils.py
@@ -40,7 +40,7 @@ def split_and_save_tensor(map_file, save_dir, minPercent=0, maxPercent=99.999, b
     map_shape = map.shape
     padded_map = np.full((map_shape[0] + 2 * box_size, map_shape[1] + 2 * box_size, map_shape[2] + 2 * box_size), 0.0, dtype=np.float32)
     padded_map[box_size : box_size + map_shape[0], box_size : box_size + map_shape[1], box_size : box_size + map_shape[2]] = map
-
+    padded_map = torch.from_numpy(padded_map)
     n_chunks = 0
     start_point = box_size - stride
     cur_x, cur_y, cur_z = start_point, start_point, start_point
@@ -56,10 +56,6 @@ def split_and_save_tensor(map_file, save_dir, minPercent=0, maxPercent=99.999, b
                 cur_z += stride
                 cur_y = start_point # Reset Y
                 cur_x = start_point # Reset X
-        if  (np.sum(next_chunk) <= 0 ):
-            print(np.sum(next_chunk))
-            print(f"all 0 tensor delete it")
-            continue
         np.savez_compressed(filename, next_chunk)
         print(f"successfully save {filename}.npz")
         n_chunks += 1
@@ -89,7 +85,100 @@ def diagonal_resample(batch_chunks, kernel):
     return batch_chunks
 
 
+def fourier_inter(batch_chunks, scale_factor):
+    """
+    SN2N tool: Fourier re-scale
+    ------
+    image_stack
+        image TO Fourier interpolation
+    
+    Returns
+    -------
+    imgf1: image with 2x size 
+    """
+    chunk_shape = batch_chunks.shape
+    print(chunk_shape)
 
+    [t, x, y] = image_stack.shape
+    imgf1 = np.zeros((t, imsize[0], imsize[1]))
+    
+    for slice in range(t):
+        img = image_stack[slice, :, :]
+        imgsz = np.array([x, y])
+        tem1 = np.divide(imgsz, 2)
+        tem2 = np.multiply(tem1, 2)
+        tem3 = np.subtract(imgsz, tem2)
+        b = (tem3 == np.array([0, 0]))
+        if b[0] == True:
+            sz = imgsz - 1
+        else:
+            sz = imgsz            
+        n = np.array([2, 2])
+        ttem1 = np.add(np.ceil(np.divide(sz, 2)), 1)
+        ttem2 = np.multiply(np.floor(np.divide(sz, 2)), np.subtract(n, 1))
+        idx = np.add(ttem1, ttem2)
+        padsize = np.array([x/2, y/2], dtype = 'int')
+        pad_wid = np.ceil(padsize[0]).astype('int')
+        img = np.pad(img, ((pad_wid, 0), (pad_wid, 0)), 'symmetric')
+        img = np.pad(img, ((0, pad_wid), (0, pad_wid)),  'symmetric')
+        imgsz1 = np.array(img.shape)
+        tttem1 = np.multiply(n, imgsz1)
+        tttem2 = np.subtract(n, 1)
+        newsz = np.round(np.subtract(tttem1, tttem2))
+        img1 = self.interpft(img, newsz[0], 0)
+        img1 = self.interpft(img1, newsz[1], 1)
+        idx = idx.astype('int')
+        ttttem1 = np.subtract(np.multiply(n[0], imgsz[0]), 1).astype('int')
+        ttttem2 = np.subtract(np.multiply(n[1], imgsz[1]), 1).astype('int')
+        imgf1[slice, :, :] = img1[idx[0] - 1:idx[0] + ttttem1, idx[1] - 1:idx[1] + ttttem2]
+        imgf1[imgf1 < 0] = 0
+    return imgf1
+
+def interpft(self, x, ny, dim = 0):
+    '''
+    Function to interpolate using FT method, based on matlab interpft()
+    ------
+    x 
+        array for interpolation
+    ny 
+        length of returned vector post-interpolation
+    dim
+        performs interpolation along dimension DIM
+        {default: 0}
+    Returns
+    -------
+    y: interpolated data
+    '''
+
+    if dim >= 1: 
+    #if interpolating along columns, dim = 1
+        x = np.swapaxes(x,0,dim)
+    #temporarily swap axes so calculations are universal regardless of dim
+    if len(x.shape) == 1:            
+    #interpolation should always happen along same axis ultimately
+        x = np.expand_dims(x,axis=1)
+
+    siz = x.shape
+    [m, n] = x.shape
+
+    a = np.fft.fft(x,m,0)
+    nyqst = int(np.ceil((m+1)/2))
+    b = np.concatenate((a[0:nyqst,:], np.zeros(shape=(ny-m,n)), a[nyqst:m, :]),0)
+
+    if np.remainder(m,2)==0:
+        b[nyqst,:] = b[nyqst,:]/2
+        b[nyqst+ny-m,:] = b[nyqst,:]
+
+    y = np.fft.irfft(b,b.shape[0],0)
+    y = y * ny / m
+    y = np.reshape(y, [y.shape[0],siz[1]])
+    y = np.squeeze(y)
+
+    if dim >= 1:  
+    #switches dimensions back here to get desired form
+        y = np.swapaxes(y,0,dim)
+
+    return y
 
 
 
